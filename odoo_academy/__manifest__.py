0fc465afc47f28857f4825c13d9bb2e0afe40ee1
David Miguel Garibay Rivera <dgaribay@crucialsoft.com.mx>
2021-07-12 16:33:59 +0000

    [ADD]Version Inicial del Odoo_Academy 03

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name' : 'odoo Academy',
+    'summary' : """Academy app training""",
+    'description' :"""
+        Academia de Cursos
+    """,
+    'author' : 'David Miguel Garibay Rivera. Crucialsoft',
+    'website': 'http://www.crucialsoft.com.mx'
+    'category' : 'Training',
+    'version' : '0.0.1',
+    'depends' : ['base']
+    'data' : [
+        
+    ],
+    'demo': [
+        
+    ],
+
+}
