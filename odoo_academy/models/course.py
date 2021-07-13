e337c1c3924cc9536d38dd4f7fe8644d1e54ef2a
David Miguel Garibay Rivera <dgaribay@crucialsoft.com.mx>
2021-07-13 10:10:54 -0500

    Se agregan campos al modelo "Course"

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -6,3 +6,14 @@ class Course(models.Model):
 
     _name = 'academy.course'
     _description = 'Informacion del Curso'
+
+    name = fields.Char(string='Título', required=True)
+    description = fields.Text(string='Descripción')
+
+    level = fields.Selection(string='Nivel',
+                             selection=[('begginer','Principiante'),
+                                        ('intermediate','Intermedio'),
+                                        ('advanced','Avanzado')],
+                             copy=False)
+							 
+    active = fields.Boolean(string='Activo', default=True)
