db9e7116fb5d00416477817afdd81628ac3893a1
David Miguel Garibay Rivera <dgaribay@crucialsoft.com.mx>
2021-07-21 14:06:46 -0500

    Curso Día 08. Modelo transitorio

--- odoo_academy/wizard/sale_wizard.py
+++ odoo_academy/wizard/sale_wizard.py
@@ -28,7 +28,6 @@ class SaleWizard(models.TransientModel):
             for student in self.student_ids:
                 order_id=self.env['sale.order'].create({
                     'partner_id': student.id,
-                    'session_id': self.session_id,
+                    'session_id': self.session_id.id,
                     'order_line': [(0,0, {'product_id': session_product_id.id, 'price_unit': self.session_id.total_price})]
-					
-})
+                })
